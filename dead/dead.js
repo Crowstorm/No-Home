3716c2cb14c291d1891c2d5e237c3d7688071c87
Big Paws <crowstormofficial@gmail.com>
2017-07-07 20:06:11 +0200

    minor fixes 2nd level remove inline interface improvments

--- dead/dead.js
+++ dead/dead.js
@@ -2,7 +2,7 @@
 document.getElementById("interface").style.display = "none";
 document.getElementById("entrance").style.display = "none";
 document.getElementById("locked").style.display = "none";
-document.getElementById("vid_locked").style.display = "none";
+// document.getElementById("vid_locked").style.display = "none";   Add after videos finished
 document.getElementById("corridor").style.display = "none";
 document.getElementById("torture").style.display = "none";
 document.getElementById("beds").style.display = "none";
@@ -109,13 +109,13 @@ function enter(){
 function locked(){
 	document.getElementById("locked").style.display = "block";
 	document.getElementById("entrance").style.display = "none";
-	document.getElementById("vid_entrance").style.display = "none";
-	document.getElementById("vid_locked").style.display = "block";
+	// document.getElementById("vid_entrance").style.display = "none";
+	//Add after videos finished
+	// document.getElementById("vid_locked").style.display = "block";
 }
 
 function locked_down(){
-	// document.getElementById("vid_entrance").style.display = "block";   NIE WIEM CZY POTRZEBNE
-	document.getElementById("vid_locked").style.display = "none";
+	// document.getElementById("vid_locked").style.display = "none";
 	document.getElementById("entrance").style.display = "block";
 	document.getElementById("locked").style.display = "none";
 	boltcutters_used = false;
@@ -154,4 +154,64 @@ function beds_down(){
 
 document.getElementById('hallway').onmousedown = function(){
 	alert("I'm not going there, I can feel a wicked presence inside");
+}
+
+//============================================
+//Event listeners
+//============================================
+
+
+//Title screen
+document.getElementById('title-button').onmousedown = function(){
+	enter();
+}
+
+//First screen
+document.getElementById('up_entrance').onmousedown = function(){
+	locked();
+}
+
+document.getElementById('down_entrance').onmousedown = function(){
+	corridor();
+}
+
+//Locked doors (exit)
+document.getElementById('down_locked').onmousedown = function(){
+	locked_down();
+}
+
+document.getElementById('chained_door').onmousedown = function(){
+	door_locked();
+}
+
+//Corridor
+document.getElementById('corridor_d').onmousedown = function(){
+	corridor_down();
+}
+
+document.getElementById('block_div').onmousedown = function(){
+	jammed.play();
+	lock_jammed();
+}
+
+document.getElementById('beds_div').onmousedown = function(){
+	prisonDoor.play();
+	beds();
+}
+
+document.getElementById('torture_div').onmousedown = function(){
+	prisonDoor.play();
+	torture_room();
+}
+
+//Torture Room
+document.getElementById('down_torture').onmousedown = function(){
+	doorBack.play()
+	torture_down();
+}
+
+//Beds
+document.getElementById('down_beds').onmousedown = function(){
+	doorBack.play()
+	beds_down();
 }
